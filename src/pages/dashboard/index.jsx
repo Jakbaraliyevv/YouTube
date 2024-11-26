0ee0e0fee858307c3d53fa0c654a8f4491bbc7f6
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2024-11-26 12:07:36 +0500

    Salom

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -11,111 +11,43 @@ import { faFaceFrown } from "@fortawesome/free-solid-svg-icons";
 import "./dashboard.scss";
 import Navbar from "../../components/navbar";
 function Dashboard() {
-  // return (
-  //   <div className="dashboard__all">
-  //     <Navbar />
-
-  //     <div className="dashboard__left__all">
-  //       <div className="dashboard__left">
-  //         <Link className="dashbord__style" to={"/dashboard"}>
-  //           <FontAwesomeIcon icon={faHouse} className="icon" />
-  //           <h3>Home</h3>
-  //         </Link>
-  //         <Link className="dashbord__style" to={"/dashboard/explor"}>
-  //           <FontAwesomeIcon icon={faCompass} className="icon" />
-  //           <h3>Explor</h3>
-  //         </Link>
-  //         <Link className="dashbord__style" to={"/dashboard/students"}>
-  //           <FontAwesomeIcon icon={faFileVideo} className="icon" />
-  //           <h3>Shorts</h3>
-  //         </Link>
-  //         <Link className="dashbord__style" to={"/dashboard/sub"}>
-  //           <FontAwesomeIcon icon={faLayerGroup} className="icon" />
-  //           <h3>Subscriptions</h3>
-  //         </Link>
-  //       </div>
-  //       <div className="border"></div>
-  //       <div className="you">
-  //         <Link className="dashbord__style" to={"/dashboard/you"}>
-  //           <FontAwesomeIcon icon={faCircleUser} className="icon" />
-  //           <h3>You</h3>
-  //         </Link>
-  //         <Link className="dashbord__style" to={"/dashboard/history"}>
-  //           <FontAwesomeIcon icon={faIdCardClip} className="icon" />
-  //           <h3>History</h3>
-  //         </Link>
-  //         <Link className="dashbord__style" to={"/dashboard/libery"}>
-  //           <FontAwesomeIcon icon={faFaceFrown} className="icon" />
-  //           <h3>Libery</h3>
-  //         </Link>
-  //       </div>
-  //     </div>
-  //     <div className="dashboard__right">
-  //       <Outlet />
-  //     </div>
-  //   </div>
-  // );
-
-  const navigate = useNavigate();
-
   return (
     <div className="dashboard__all">
       <Navbar />
 
       <div className="dashboard__left__all">
         <div className="dashboard__left">
-          <div
-            className="dashbord__style"
-            onClick={() => navigate("/dashboard")}
-          >
+          <Link className="dashbord__style" to={"/dashboard"}>
             <FontAwesomeIcon icon={faHouse} className="icon" />
             <h3>Home</h3>
-          </div>
-          <div
-            className="dashbord__style"
-            onClick={() => navigate("/dashboard/explor")}
-          >
+          </Link>
+          <Link className="dashbord__style" to={"/dashboard/explor"}>
             <FontAwesomeIcon icon={faCompass} className="icon" />
             <h3>Explor</h3>
-          </div>
-          <div
-            className="dashbord__style"
-            onClick={() => navigate("/dashboard/students")}
-          >
+          </Link>
+          <Link className="dashbord__style" to={"/dashboard/students"}>
             <FontAwesomeIcon icon={faFileVideo} className="icon" />
             <h3>Shorts</h3>
-          </div>
-          <div
-            className="dashbord__style"
-            onClick={() => navigate("/dashboard/sub")}
-          >
+          </Link>
+          <Link className="dashbord__style" to={"/dashboard/sub"}>
             <FontAwesomeIcon icon={faLayerGroup} className="icon" />
             <h3>Subscriptions</h3>
-          </div>
+          </Link>
         </div>
         <div className="border"></div>
         <div className="you">
-          <div
-            className="dashbord__style"
-            onClick={() => navigate("/dashboard/you")}
-          >
+          <Link className="dashbord__style" to={"/dashboard/you"}>
             <FontAwesomeIcon icon={faCircleUser} className="icon" />
             <h3>You</h3>
-          </div>
-          <div
-            className="dashbord__style"
-            onClick={() => navigate("/dashboard/history")}
-          >
+          </Link>
+          <Link className="dashbord__style" to={"/dashboard/history"}>
             <FontAwesomeIcon icon={faIdCardClip} className="icon" />
             <h3>History</h3>
-          </div>
-          <div
-            className="dashbord__style"
-            onClick={() => navigate("/dashboard/libery")}
-          >
+          </Link>
+          <Link className="dashbord__style" to={"/dashboard/libery"}>
             <FontAwesomeIcon icon={faFaceFrown} className="icon" />
             <h3>Libery</h3>
-          </div>
+          </Link>
         </div>
       </div>
       <div className="dashboard__right">
@@ -123,6 +55,74 @@ function Dashboard() {
       </div>
     </div>
   );
+
+  // const navigate = useNavigate();
+
+  // return (
+  //   <div className="dashboard__all">
+  //     <Navbar />
+
+  //     <div className="dashboard__left__all">
+  //       <div className="dashboard__left">
+  //         <div
+  //           className="dashbord__style"
+  //           onClick={() => navigate("/dashboard")}
+  //         >
+  //           <FontAwesomeIcon icon={faHouse} className="icon" />
+  //           <h3>Home</h3>
+  //         </div>
+  //         <div
+  //           className="dashbord__style"
+  //           onClick={() => navigate("/dashboard/explor")}
+  //         >
+  //           <FontAwesomeIcon icon={faCompass} className="icon" />
+  //           <h3>Explor</h3>
+  //         </div>
+  //         <div
+  //           className="dashbord__style"
+  //           onClick={() => navigate("/dashboard/students")}
+  //         >
+  //           <FontAwesomeIcon icon={faFileVideo} className="icon" />
+  //           <h3>Shorts</h3>
+  //         </div>
+  //         <div
+  //           className="dashbord__style"
+  //           onClick={() => navigate("/dashboard/sub")}
+  //         >
+  //           <FontAwesomeIcon icon={faLayerGroup} className="icon" />
+  //           <h3>Subscriptions</h3>
+  //         </div>
+  //       </div>
+  //       <div className="border"></div>
+  //       <div className="you">
+  //         <div
+  //           className="dashbord__style"
+  //           onClick={() => navigate("/dashboard/you")}
+  //         >
+  //           <FontAwesomeIcon icon={faCircleUser} className="icon" />
+  //           <h3>You</h3>
+  //         </div>
+  //         <div
+  //           className="dashbord__style"
+  //           onClick={() => navigate("/dashboard/history")}
+  //         >
+  //           <FontAwesomeIcon icon={faIdCardClip} className="icon" />
+  //           <h3>History</h3>
+  //         </div>
+  //         <div
+  //           className="dashbord__style"
+  //           onClick={() => navigate("/dashboard/libery")}
+  //         >
+  //           <FontAwesomeIcon icon={faFaceFrown} className="icon" />
+  //           <h3>Libery</h3>
+  //         </div>
+  //       </div>
+  //     </div>
+  //     <div className="dashboard__right">
+  //       <Outlet />
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default Dashboard;
